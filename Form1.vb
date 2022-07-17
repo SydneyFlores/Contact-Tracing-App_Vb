0fa79570c8b9a499688fb637b7a43d1b2b7a5f00
SydneyFlores <mharsydney@gmail.com>
2022-07-17 23:28:24 +0800

    camera display working fine

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,14 @@
 ﻿Imports System.IO
+Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+
 
 
 Public Class Form1
     Dim txtwriter As StreamWriter
+    Dim Display As VideoCaptureDevice
+    Dim bitDisplay As Bitmap
 
 
 
@@ -19,5 +25,20 @@ Public Class Form1
 
         txtwriter.Close()
         MessageBox.Show("Successfully Added")
+    End Sub
+
+    Private Sub btnopen_Click(sender As Object, e As EventArgs) Handles btnopen.Click
+        Dim camDisplay As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+        If camDisplay.ShowDialog() = DialogResult.OK Then
+            Display = camDisplay.VideoDevice
+            AddHandler Display.NewFrame, New NewFrameEventHandler(AddressOf PictureBoxDisplay)
+            Display.Start()
+
+        End If
+    End Sub
+
+    Private Sub PictureBoxDisplay(sender As Object, eventArgs As NewFrameEventArgs)
+        bitDisplay = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        CameraDisplay.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
     End Sub
 End Class
